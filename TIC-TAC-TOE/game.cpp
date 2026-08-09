7670971c07b686612feae6348135632323a15fb4
Shikhar Shrivastav <shrishikhar1709@gmail.com>
2026-08-10 00:36:01 +0530

    added the board and basic classes to it

--- TIC-TAC-TOE/game.cpp
+++ TIC-TAC-TOE/game.cpp
@@ -26,3 +26,36 @@ class Symbol {
             return mark;
         }
 };
+
+//Board class - manages the grids
+class Board {
+    private:
+        vector<vector<Symbol>> grid;
+        int size;
+        Symbol* emptyCell;
+    
+    public:
+        Board(int s) {
+            size = s;
+            emptyCell = new Symbol('-');
+            grid.resize(size, vector<Symbol>(size, *emptyCell));
+        }
+
+        bool isCellEmpty(int row, int col) {
+            if(row < 0 || row > size || col < 0 || col >= size) return false;
+            return grid[row][col].getMark() == emptyCell->getMark();
+        }
+
+
+        bool placeMark(int row, int col, Symbol* mark){
+            if(row < 0 || row >= size || col < 0 || col >= size) return false;
+
+            if(!isCellEmpty(row, col)) return false;
+
+            grid[row][col] = *mark;
+            return true;
+        }
+
+};
+
+
