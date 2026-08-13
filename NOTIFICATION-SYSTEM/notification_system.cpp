14f6d7d40166097679fdcebec3c15504c3ccd861
Shikhar Shrivastav <shrishikhar1709@gmail.com>
2026-08-13 14:27:59 +0530

    user entity id

--- NOTIFICATION-SYSTEM/notification_system.cpp
+++ NOTIFICATION-SYSTEM/notification_system.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <memory>
 #include <chrono>
-
+using namespace std;
 //enums
 enum class NotificationChannelType {
     EMAIL,
@@ -16,5 +16,28 @@ enum class MessageType {
     DIRECT,
     GROUP
 };
+
+
+//user entities
+class User {
+private:
+    string id;
+    string name;
+    string email;
+    string phoneNumber;
+    vector<NotificationChannelType> preferredChannels;
+public:
+    User(string id, string name, string email, string phoneNumber, vector<NotificationChannelType> channels)
+        : id(id), name(name), email(email), phoneNumber(phoneNumber), preferredChannels(channels) {}
+
+    //getters
+    string getId() const {return id;}
+    string getName() const {return name;}
+    string getEmail() const {return email;}
+    string getPhoneNumber() const {return phoneNumber;}
+    vector<NotificationChannelType> getPreferredChannels() const {return preferredChannels;}
+};
+
+
 
 
