b49adf75ba63b669ff27bdef9756a0b8c35873e8
Shikhar Shrivastav <shrishikhar1709@gmail.com>
2026-08-13 14:20:08 +0530

    THE ENUMS ARE UPDATED

---
+++ NOTIFICATION-SYSTEM/notification_system.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <chrono>
+
+//enums
+enum class NotificationChannelType {
+    EMAIL,
+    SMS,
+    IN_APP,
+    PUSH
+};
+
+enum class MessageType {
+    DIRECT,
+    GROUP
+};
+
+
